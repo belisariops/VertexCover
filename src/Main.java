62e300f78d7d6ebad6cd06a44423f275a84836b9
Américo Igancio Ferrada Diaz <americo.ferrada@gmail.com>
2017-07-05 23:44:26 -0400

    se agrego encabezado csv

--- src/Main.java
+++ src/Main.java
@@ -27,84 +27,85 @@ public class Main {
         double p;
         double[] pArray = new double[]{4, 16, 64, 256, 1024};
         String fileName;
-        String fileNameData;
+        String fileNameData = "ExperimentoData.csv";
         double constructionTime,twoAproximationTime,maximumDegreeHeuristicTime,improvedTwoAproximationTime;
 
-        for (int i= 10; i<=10; i++) {
-            n = (int) Math.pow(2, i);
-            fileName = "Experimento_i" + i + ".txt";
-            fileNameData = "Experimento_i" + i + "Data.csv";
-            path = Paths.get(fileName);
-            pathData = Paths.get(fileNameData);
-            try (BufferedWriter writer = Files.newBufferedWriter(path);
-                 BufferedWriter writerData = Files.newBufferedWriter(pathData)) {
-                writerData.write("NumVertices,NumGrafo,Probabilidad,tiempoContruccion,Tiempo2Aproximacion,TiempoMaximumDegree,Tiempo2ApriximacionMejorada");
-                writerData.newLine();
-                for (int j = 0; j < 3; j++) {
-                    for (int k = 0; k < 5; k++) {
-                        p = pArray[k]/n;
-                        timer.start();
-                        g1 = generator.create(n, p);
-                        constructionTime = timer.stop();
-                        g2 = new Graph(g1);
-                        g3 = new Graph(g1);
+        pathData = Paths.get(fileNameData);
+        try(BufferedWriter writerData = Files.newBufferedWriter(pathData)) {
+            writerData.write("NumVertices,NumGrafo,Probabilidad,tiempoContruccion,Tiempo2Aproximacion,TiempoMaximumDegree,Tiempo2ApriximacionMejorada");
+            writerData.newLine();
+            for (int i = 10; i <= 10; i++) {
+                n = (int) Math.pow(2, i);
+                fileName = "Experimento_i" + i + ".txt";
+                path = Paths.get(fileName);
+                try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+                    for (int j = 0; j < 3; j++) {
+                        for (int k = 0; k < 5; k++) {
+                            p = pArray[k] / n;
+                            timer.start();
+                            g1 = generator.create(n, p);
+                            constructionTime = timer.stop();
+                            g2 = new Graph(g1);
+                            g3 = new Graph(g1);
 
-                        timer.start();
-                        twoAproximation = new TwoAproximation(g1);
-                        twoAproximationTime = timer.stop();
+                            timer.start();
+                            twoAproximation = new TwoAproximation(g1);
+                            twoAproximationTime = timer.stop();
 
-                        timer.start();
-                        maximumDegreeHeuristic = new MaximumDegreeHeuristic(g2);
-                        maximumDegreeHeuristicTime = timer.stop();
+                            timer.start();
+                            maximumDegreeHeuristic = new MaximumDegreeHeuristic(g2);
+                            maximumDegreeHeuristicTime = timer.stop();
 
-                        timer.start();
-                        improvedTwoAproximation = new ImprovedTwoAproximation(g3);
-                        improvedTwoAproximationTime = timer.stop();
+                            timer.start();
+                            improvedTwoAproximation = new ImprovedTwoAproximation(g3);
+                            improvedTwoAproximationTime = timer.stop();
 
-                        writerData.write(n+","+j+","+p+","+constructionTime+","+twoAproximationTime+","+maximumDegreeHeuristicTime+","+improvedTwoAproximationTime);
-                        writerData.newLine();
-                        writer.write("Tiempo Construccion Grafo con p="+p+", "+g1.getV().size()+" nodos, "+g1.getE().size()/2+" aristas, construido en "+constructionTime+" segundos.");
+                            writerData.write(n + "," + j + "," + p + "," + constructionTime + "," + twoAproximationTime + "," + maximumDegreeHeuristicTime + "," + improvedTwoAproximationTime);
+                            writerData.newLine();
+                            writer.write("Tiempo Construccion Grafo con p=" + p + ", " + g1.getV().size() + " nodos, " + g1.getE().size() / 2 + " aristas, construido en " + constructionTime + " segundos.");
+                            writer.newLine();
+                            writer.newLine();
+                            writer.write("2-Aproximacion:");
+                            writer.newLine();
+                            writer.write("                 Cantidad de vertices solucion:" + twoAproximation.getVertexCoverSize());
+                            writer.newLine();
+                            writer.write("                 Tiempo de ejecucion:" + twoAproximationTime + " segundos.");
+                            writer.newLine();
+                            writer.newLine();
+                            writer.write("Heuristica de grado mayor:");
+                            writer.newLine();
+                            writer.write("                 Cantidad de vertices solucion:" + maximumDegreeHeuristic.getVertexCoverSize());
+                            writer.newLine();
+                            writer.write("                 Tiempo de ejecucion:" + maximumDegreeHeuristicTime + " segundos.");
+                            writer.newLine();
+                            writer.newLine();
+                            writer.write("2-Aproximacion mejorada:");
+                            writer.newLine();
+                            writer.write("                 Cantidad de vertices solucion:" + improvedTwoAproximation.getVertexCoverSize());
+                            writer.newLine();
+                            writer.write("                 Tiempo de ejecucion:" + improvedTwoAproximationTime + " segundos.");
+                            writer.newLine();
+                            writer.newLine();
+                            writer.write("------------------------------------------------------------------------------------------------------------------------------------");
+                            writer.newLine();
+
+                        }
                         writer.newLine();
+                        writer.write("***********************************************************************************************************************************");
+                        writer.write("***********************************************************************************************************************************");
                         writer.newLine();
-                        writer.write("2-Aproximacion:");
-                        writer.newLine();
-                        writer.write("                 Cantidad de vertices solucion:"+twoAproximation.getVertexCoverSize());
-                        writer.newLine();
-                        writer.write("                 Tiempo de ejecucion:"+twoAproximationTime+" segundos.");
-                        writer.newLine();
-                        writer.newLine();
-                        writer.write("Heuristica de grado mayor:");
-                        writer.newLine();
-                        writer.write("                 Cantidad de vertices solucion:"+maximumDegreeHeuristic.getVertexCoverSize());
-                        writer.newLine();
-                        writer.write("                 Tiempo de ejecucion:"+maximumDegreeHeuristicTime+" segundos.");
-                        writer.newLine();
-                        writer.newLine();
-                        writer.write("2-Aproximacion mejorada:");
-                        writer.newLine();
-                        writer.write("                 Cantidad de vertices solucion:"+improvedTwoAproximation.getVertexCoverSize());
-                        writer.newLine();
-                        writer.write("                 Tiempo de ejecucion:"+improvedTwoAproximationTime+" segundos.");
-                        writer.newLine();
-                        writer.newLine();
-                        writer.write("------------------------------------------------------------------------------------------------------------------------------------");
                         writer.newLine();
 
                     }
-                    writer.newLine();
-                    writer.write("***********************************************************************************************************************************");
-                    writer.write("***********************************************************************************************************************************");
-                    writer.newLine();
-                    writer.newLine();
+                    writer.close();
 
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                writer.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 }
 
